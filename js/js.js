1f7429512090e2ea20ac0aa15f03dd685e296673
Nabil <nabilashraf679@gmail.com>
2021-10-05 22:43:56 +0530

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,25 @@
+// contacts = document.getElementById('contact-js').innerHTML;
+
+//  var box1  =  document.getElementsByClassName('contact').innerHTML;
+// contacts.addEventListener("click",function(){
+      
+//        if(box1.style.display=='none'){
+//            box1.style.contact="block";
+//        }
+//         else{
+//             box1.style.contact="none";
+//         } 
+// })
+
+// document.getElementById("contact-js").addEventListener("click",function()
+// {
+//     let smg = document.getElementsByClassName("contact");
+//     if(smg.style.contact=="none")
+//     {
+//         smg.style.contact="block";
+//     }
+//     else
+//     {
+//         smg.style.contact="none";
+//     }
+// })
